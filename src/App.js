630ed455a15685ed6deea6e9b9789bad8c6efa02
swathi@18 <harnadh191295@gmail.com>
2021-03-02 14:32:02 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from './component/Home';
+import Footer from './component/Footer';
+import About from './component/About';
+import Nav from './component/Nav';
+import Skills from './component/Skills';
+import Work from './component/Work';
+import Contact from './component/Contact';
+import './App.css';
+
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Nav/>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/skills" component={Skills} />
+          <Route exact path="/work" component={Work} />
+          <Route exact path="/contact" component={Contact} />
+        </Switch>
+        <Footer/>
+      
+    </div>
+    </Router>
+    
+  );
+}
+
+export default App;
