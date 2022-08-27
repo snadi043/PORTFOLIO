d49afae1469f686e0c69d999f14359c0d27363c6
SAI HARNADH NADIPALLI <snadi043@uottawa.ca>
2022-08-27 10:54:19 -0400

    refactored repeated code

--- src/component/About/index.js
+++ src/component/About/index.js
@@ -42,34 +42,26 @@ const About = () => {
               </div>
             </div>
         </div>
-            <p>
-              • Efficient Web Developer with 2 years of professional experience in Web development technologies and  Agile Software Development Lifecycles with good understanding in client requirements. <br/><br/>
-              • Experience in developing effective User Interfaces using HTML/HTML5, CSS, SASS, Bootstrap, PHP, jQuery, JavaScript, AJAX, JSON, ReactJS and NodeJS.<br/><br/>
-              • Experience in implementing JavaScript (ES6) features like Object Oriented concepts and arrow functions.<br/><br/>
-              • Expertise in writing SQL quires, creating database tables and implementing CRUD operations on database servers like Apache Tomcat, Xampp and phpMyAdmin.<br/><br/>
-              • Experienced in designing websites with CMS (Content Management System) tools like WordPress.<br/><br/>
-              • Good understanding of NOSQL databases like MongoDB.<br/><br/>
-              • Experience in using distributed version control tools like Git and GitHub for maintaining versions of source code. <br/><br/>
-              • Experience in Docker containerization and customization of Docker images. <br/><br/>
-              • Experience in building Continuous Integration (CI) /Continuous Deployment (CD) pipelines for application requirements using Jenkins. <br/><br/>
-              • Implemented projects in software development methodologies such as Agile, Scrum and Waterfall. <br/><br/>
-              • Experience in using Integrated Development Environments (IDE) such as Eclipse, Visual Studio and Sublime Text 3.<br/><br/>
-              • Experience in working with project tracking tools like JIRA, Freed Camp and Microsoft Visio. <br/><br/><br/>
+            <p>• Efficient Web Developer with 2 years of professional experience in Web development technologies and  Agile Software Development Lifecycles with good understanding in client requirements.</p>
+            <p>• Experience in developing effective User Interfaces using HTML/HTML5, CSS, SASS, Bootstrap, PHP, jQuery, JavaScript, AJAX, JSON, ReactJS and NodeJS.</p>
+            <p>• Experience in implementing JavaScript (ES6) features like Object Oriented concepts and arrow functions.</p>
+            <p>• Expertise in writing SQL quires, creating database tables and implementing CRUD operations on database servers like Apache Tomcat, Xampp and phpMyAdmin.</p>
+            <p>• Experienced in designing websites with CMS (Content Management System) tools like WordPress.</p>
+            <p>• Good understanding of NOSQL databases like MongoDB.</p>
+            <p>• Experience in using distributed version control tools like Git and GitHub for maintaining versions of source code. </p>
+            <p>• Experience in Docker containerization and customization of Docker images. </p>
+            <p>• Experience in building Continuous Integration (CI) /Continuous Deployment (CD) pipelines for application requirements using Jenkins. </p>
+            <p>• Implemented projects in software development methodologies such as Agile, Scrum and Waterfall. </p>
+            <p>• Experience in using Integrated Development Environments (IDE) such as Eclipse, Visual Studio and Sublime Text 3.</p>
+            <p>• Experience in working with project tracking tools like JIRA, Freed Camp and Microsoft Visio. </p>
+            <p></p>
+              
+              
+             
+             
 
-              • Efficient Web Developer with 2 years of professional experience in Web development technologies and  Agile Software Development Lifecycles with good understanding in client requirements. <br/><br/>
-              • Experience in developing effective User Interfaces using HTML/HTML5, CSS, SASS, Bootstrap, PHP, jQuery, JavaScript, AJAX, JSON, ReactJS and NodeJS.<br/><br/>
-              • Experience in implementing JavaScript (ES6) features like Object Oriented concepts and arrow functions.<br/><br/>
-              • Expertise in writing SQL quires, creating database tables and implementing CRUD operations on database servers like Apache Tomcat, Xampp and phpMyAdmin.<br/><br/>
-              • Experienced in designing websites with CMS (Content Management System) tools like WordPress.<br/><br/>
-              • Good understanding of NOSQL databases like MongoDB.<br/><br/>
-              • Experience in using distributed version control tools like Git and GitHub for maintaining versions of source code. <br/><br/>
-              • Experience in Docker containerization and customization of Docker images. <br/><br/>
-              • Experience in building Continuous Integration (CI) /Continuous Deployment (CD) pipelines for application requirements using Jenkins. <br/><br/>
-              • Implemented projects in software development methodologies such as Agile, Scrum and Waterfall. <br/><br/>
-              • Experience in using Integrated Development Environments (IDE) such as Eclipse, Visual Studio and Sublime Text 3.<br/><br/>
-              • Experience in working with project tracking tools like JIRA, Freed Camp and Microsoft Visio. <br/><br/>
 
-            </p>
+            
     </div>
   </div>
       </>
