bd4081843d32ba531b4723c0b4b8e00052e170dd
SAI HARNADH NADIPALLI <snadi043@uottawa.ca>
2022-08-29 12:03:55 -0400

    added recent work and stylings

--- src/component/Work/index.js
+++ src/component/Work/index.js
@@ -4,7 +4,51 @@ import './style.scss';
 const Work = () => {
     return (
     <div className="works">
-      <h1>WORK EXPERIENCE</h1>
+        <h1>WORK EXPERIENCE</h1>
+
+      <p>Company: Aethon Aerial Solutions</p>
+      <p>Position: FRONTEND DEVELOPER</p>
+      <p>Date: Jul 2021 – Jul 2022</p>
+        <p>Worked closely with the product owner to create new components which can be reused in different projects using shared libraries.</p>
+        <p>Refactoring the existing code in Angular 6/8 to increase the feasibility performance of the application.</p>
+        <p>Implemented migration of MSAL authentication to AWS Cognito using HostedUI for login screen.</p>
+        <p>Worked with AWS Amplify and Identity JS libraries and added user management groups and their permissions to access and update them through UI.</p>
+        <p>Created shared libraries which can be used in a different angular application on a different container.</p>
+        <p>Added SSL certifications to enable cross communication between two applications for reusability of API's.</p>
+        <p>Used Angular NX and created workspace flexible to manage multiple libraries, modules and dependencies.</p>
+        <p>Used Figma for creating mockups, wireframes and implemented them in the applications.</p>
+        <p>Refactored GraphQL queries with SQL and HTTP Client requests for centralized database access.</p>
+        <p>Worked closely with backend developer in creating new C# controllers, models for data mapping in API's and involved in refactoring the database to improve efficiency.</p>
+        <p>Involved in enhancing SQL queries with LINQ in .NET framework and reduced API latency (10 sec - 1 sec).</p>
+        <p>Setting up legacy application which runs on Python and Docker by installing all dependencies without any documentation in Linux and Ubuntu systems.</p>
+        <p>Experience in working with Test-Driven Development and Feature-Driven Development concepts.</p>
+        <p>Published npm packages locally so that it can be used by another applications so it reduces code redundancy.</p>
+        <p>Implemented frontend feature for exporting CSV files on a button click on mat-table.</p>
+        <p>Implemented multi select dropdown on mat-select as per client requirements.</p>
+        <p>Contributed in resolving the build errors while running the pipelines, generated automated test results and reflected the changes in production environment in time.</p>
+        <p>Implemented React containers and presentational components (as Stateless and Functional components).</p>
+        <p>Implemented sorting, filtering functionalities over the tabular logs developed in MaterialUI and ReactJS.</p>
+        <p>Presented POC for the whole SPA and translated the existing code with new changes as per the requirements.</p>
+        <p>Implemented multiple frontend UI's using ReactJS and used various predefined components from NPM (Node Package Manager) and Redux library.</p>
+        <br/>
+
+      <p>Company: AspenClean</p>
+      <p>Position: FRONTEND WEB DEVELOPER</p>
+      <p>Date: Apr 2020 – Jun 2021</p>
+         <p>Implemented stable React components and stand-alone functions for future use in the projects.</p>
+         <p>Developed data models and services to implement REST API's, retrieved Data from the database to enable CRUD operations resulting user friendly frontend UI.</p>
+         <p>Configured Webpack to execute linters, pre/postprocessors, tests, transpires to run application in old browsers.</p>
+         <p>Developed GEH (Global Error Handlers) in applications which helps to understand and resolve the errors.</p>
+         <p>Involved in React UI and architecture, implemented Flux pattern with redux framework as a core dependency.</p>
+         <p>Knowledge on JavaScript based frameworks like React Native and Ionic/Cordova used in implementing mobile development features in web applications.</p>
+         <p>Used React-Autocomplete for creating google map's location search on the webpage.</p>
+         <p>Configured AWS user pools, app-client settings, OAUTH, integrated authentication and JWT tokens for user management.</p>
+         <p>Worked with JASMINE unit test framework to write unit tests for JavaScript code.</p>
+         <p>Involved in complete SDLC - Requirement Analysis, Development, System, and Integration Testing.</p>
+         <p>Involved in complete SDLC - Requirement Analysis, Development, System, and Integration Testing.</p>
+         <p>Experience in interacting with offshore team to share Functional, Technical knowledge, code reviews and to track the status of the project.</p>
+         <br/>
+
       <p>Company: Arete IT Services Private Limited</p>
       <p>Position: WEB DEVELOPER</p>
       <p>Date: Jan 2017 – Nov 2018</p>
@@ -46,7 +90,7 @@ const Work = () => {
       <p>Tools and Technologies: HTML, CSS, JavaScript, PHP and Microsoft Office Suite. </p>
     <br />
 
-    <p>FOOD LOGING APP</p><br />
+    <p>FOOD LOGING APP</p>
       <p>Position: Project Team Lead</p>
       <p>Date: Jan 2019 – April 2019</p>
       <p>Delivered Front End application featuring user profile, Camera, calorie intake and expenditure.</p>
@@ -58,7 +102,7 @@ const Work = () => {
 
       <p>VOLUNTEERY EXPERIENCE</p>
       <p>Position: Project Team Lead</p>
-      <p>Date: Jan 2019 – April 2019</p>
+      <p>Date: Jan 2016 – April 2017</p>
       <p>Member in uOttawa volunteering services – Conducted campus tours and provided housing to 30 students.</p>
       <p>Computer faculty in GEDT Government High School, Vijayawada, A.P - Worked on updating the Education details of 250 students on state government website. </p>
       <p> Volunteered for “TEXAS INSTRUMENTS” – Coordinated the faculty development program and attended seminars.</p>
