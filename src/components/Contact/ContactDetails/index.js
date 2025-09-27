4cafb496a897d6bd821c46175f8715e810cb02fe
snadi043 <snadi043@uottawa.ca>
2025-09-26 23:33:46 -0400

    Changes made to make the components more leaner by using the map() function on the data objects.

--- src/components/Contact/ContactDetails/index.js
+++ src/components/Contact/ContactDetails/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import contactDetailsData from './data';
+
 import Wrapper from '../../../Utilities/Wrapper';
 import Card from '../../../Utilities/Card';
 import IconWithText from '../../../Utilities/IconWithText';
@@ -20,42 +22,26 @@ const ContactDetails = () => {
     return (
         <Wrapper style={{padding: '60px 0px'}}>
             <Card>
-                <IconWithText
-                    id="iconWithText" 
-                    iconPath={"M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A32 32 0 0 1 8 14.58a32 32 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10 M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4m0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6"} 
-                    textDescription={"1755 Frobisher Lane, Ottawa, ON"} 
-                    textTitle={"Address"} 
-                    className={"bi bi-geo-alt"}
+                {contactDetailsData.map((data, index) => <IconWithText
+                    iconPath={data.iconPath}
+                    textTitle={data.textTitle}
+                    className={data.className}
+                    textDescription={data.textDescription}
                     svgHeight={"24px"}
                     svgWidth={"24px"}
                     iconWrapperStyle={iconWrapperStyle}
                     textTitleStyles={{fontWeight: 'bolder', fontSize: '18px'}}
                     additionalStyles={{padding: '20px'}}
-                />
-                <IconWithText
-                    id="iconWithText" 
-                    iconPath={"M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.6 17.6 0 0 0 4.168 6.608 17.6 17.6 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.68.68 0 0 0-.58-.122l-2.19.547a1.75 1.75 0 0 1-1.657-.459L5.482 8.062a1.75 1.75 0 0 1-.46-1.657l.548-2.19a.68.68 0 0 0-.122-.58zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z"} 
-                    textDescription={"343-999-2611"} 
-                    textTitle={"Call Me"} 
-                    className={"bi bi-telephone"}
-                    svgHeight={"24px"}
-                    svgWidth={"24px"}
-                    iconWrapperStyle={iconWrapperStyle}
-                    textTitleStyles={{fontWeight: 'bolder', fontSize: '18px'}}
-                    additionalStyles={{padding: '20px'}}
-                />
-                <IconWithText 
-                    id="iconWithText" 
-                    iconPath={"M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1zm13 2.383-4.708 2.825L15 11.105zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741M1 11.105l4.708-2.897L1 5.383z"} 
-                    textDescription={"nsharnadh18@gmail.com"} 
-                    textTitle={"Email Me"} 
-                    className={"bi bi-envelope"}
-                    svgHeight={"24px"}
-                    svgWidth={"24px"}
-                    iconWrapperStyle={iconWrapperStyle}
-                    textTitleStyles={{fontWeight: 'bolder', fontSize: '18px'}}
-                    additionalStyles={{padding: '20px'}}
-                />
+                />)}
+                <Wrapper style={{padding: '20px'}}>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11205.506441721562!2d-75.6746598660279!3d45.40174463176831!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4cce058dc231d1d5%3A0x3dfd1915488e9b06!2sFrobisher%20Ln%2C%20Ottawa%2C%20ON!5e0!3m2!1sen!2sca!4v1614662504731!5m2!1sen!2sca" 
+                        title="contact_title"
+                        width={"100%"} 
+                        height="250" 
+                        loading="lazy"
+                        style={{border:0}}
+                    />
+                </Wrapper>
             </Card>
         </Wrapper>
     );
